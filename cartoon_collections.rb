816206f0503dd0f39343f89cd15df14e06c6780c
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 20:00:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,9 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? { |word|
+    word.length >= 4
+  }
 end
 
 def find_the_cheese# code an argument here
