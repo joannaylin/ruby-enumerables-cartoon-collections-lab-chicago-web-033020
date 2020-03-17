2aeac3b9796f87c7af3fe62d50a18c16df5f97c9
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 20:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,10 @@ def long_planeteer_calls(array)# code an argument here
   }
 end
 
-def find_the_cheese(cheese_array)# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include { |item|
+    item == cheese_types
+  }
 end
