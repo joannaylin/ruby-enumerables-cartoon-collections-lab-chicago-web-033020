ff5eb6c4361938551e08e5ca4410af9420c0f0db
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 20:24:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,9 +22,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    return array.select { |cheese|
-      array.include?(cheese)
+  if array.find { |cheese|
+    array.include?(cheese)
     }
   end
 end
