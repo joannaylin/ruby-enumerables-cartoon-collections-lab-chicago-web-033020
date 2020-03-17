709115ed83adf4ece2e118ff3e7017b3d0999521
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 19:28:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index { |dwarf, index|
-    puts "#{(index+1)}. dwarf.capitalize"
+    puts "#{(index+1)}. dwarf.capitalize\n"
   }
 end
 
