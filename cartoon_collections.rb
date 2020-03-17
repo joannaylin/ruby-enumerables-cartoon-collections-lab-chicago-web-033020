940bb3f968d17f35648f37705200d6134133c6ed
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 19:51:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  calls.map! { |calls|
-    puts "#{calls.capitalize}!"
+  calls. { |calls|
+    calls.capitalize + "!"
   }
 end
 
